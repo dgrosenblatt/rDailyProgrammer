3f49e5890cebaca80cfb36f806ec381ed80eef8c
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-03-01 17:18:05 -0500

    Clean up tests for 202

--- i-am-bender-202/bender_spec.rb
+++ i-am-bender-202/bender_spec.rb
@@ -4,11 +4,13 @@ require_relative 'i-am-bender-202'
 RSpec.describe "translate" do
   it "should translate a binary string to english" do
     expect(translate("01001000")).to eq("H")
-    expect(translate("0100100001100101011011000110110001101111001000000101011101101111011100100110110001100100")).to eq("Hello World")
+    expect(translate("01001000011001010110110001101100011011110010000001010111\
+    01101111011100100110110001100100")).to eq("Hello World")
   end
 
   it "should translate a binary string to english in one line" do
     expect(line_translate("01001000")).to eq("H")
-    expect(line_translate("0100100001100101011011000110110001101111001000000101011101101111011100100110110001100100")).to eq("Hello World")
+    expect(line_translate("010010000110010101101100011011000110111100100000010\
+    1011101101111011100100110110001100100")).to eq("Hello World")
   end
 end
