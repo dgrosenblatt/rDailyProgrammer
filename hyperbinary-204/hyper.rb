cb8224441f8459e0d054130ece590700cf8913d5
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-03-11 23:22:27 -0400

    Initial decimal to hyper method

--- hyperbinary-204/hyper.rb
+++ hyperbinary-204/hyper.rb
@@ -8,5 +8,41 @@ def to_dec(hyper)
   sum
 end
 
+def to_hyper(dec)
+  max_power = 0
+  loop do
+    if (2**max_power) > dec
+      max_power -= 1
+      break
+    else
+      max_power += 1
+    end
+  end
 
-puts to_dec("1021")
+  max_hyper = "1" + ("0" * max_power)
+
+  possibilities = []
+
+  (0..max_hyper.to_i).each do |n|
+    if !(n.to_s.split('') & ("3".."9").to_a).any?
+      possibilities << n.to_s
+    end
+  end
+
+  solutions = []
+
+  possibilities.each do |poss|
+    if to_dec(poss) == dec
+      solutions << poss
+    end
+  end
+
+  solutions
+end
+
+
+# puts to_dec("1021")
+# puts
+# puts to_hyper(9)
+# puts to_hyper(4)
+puts to_hyper(18)
